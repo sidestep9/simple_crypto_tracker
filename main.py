13c16beef18f5cefb752254a9c77a9f929a4f344
sidestep9 <renhirtana@yahoo.com>
2026-07-06 09:58:41 +0700

    finish with loop and added time pause

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import requests, json
+import requests, json, time
 
-res = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd")
-res.raise_for_status()
+while True:
+    res = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd")
+    res.raise_for_status()
 
-res_data = json.loads(res.text)
+    res_data = json.loads(res.text)
 
-print("=== CRYPTO PRICE TRACKER ===")
-for k, v in res_data.items():
-    print(f"{k.upper()}: ${v["usd"]}")
-print("============================")
+    print("\n=== CRYPTO PRICE TRACKER ===")
+    for k, v in res_data.items():
+        print(f"{k.upper()}: ${v["usd"]}")
+    print("============================")
+    time.sleep(5)
