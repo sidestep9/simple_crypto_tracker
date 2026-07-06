529ab0806c616ff8cc5aef314599c696deb7fb47
sidestep9 <renhirtana@yahoo.com>
2026-07-06 10:45:12 +0700

    added load and save

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import requests, json, time
+import requests, time, json
 
 while True:
     res = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd")
     res.raise_for_status()
 
-    res_data = json.loads(res.text)
+    res_data = res.json()
+
+    with open("price_data.json", "r", encoding="utf-8") as file:
+        prev_data = json.load(file)
+    
+    print(prev_data)
+    
+    with open("price_data.json", "w") as file:
+        json.dump(res_data, file)
 
     print("\n=== CRYPTO PRICE TRACKER ===")
     for k, v in res_data.items():
         print(f"{k.upper()}: ${v["usd"]}")
     print("============================")
-    time.sleep(5)
+    time.sleep(30)
